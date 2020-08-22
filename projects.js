748831f36ff20495b7086987984ba0b3067a0985
Wanda <wmmcinturff@gmail.com>
2020-08-22 06:34:30 -0500

    revised projects function and got conditional logic to execute

--- projects.js
+++ projects.js
@@ -16,7 +16,7 @@ const projects = [
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   },
@@ -25,7 +25,7 @@ const projects = [
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   },
@@ -34,7 +34,7 @@ const projects = [
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
   },
@@ -49,8 +49,8 @@ const printToDom = (divId, textToPrint) => {
 
   const createProjectCards = () => {
     let domString = '';
-
-for (let i=0; i < projects.length; i++) {
+       for (let i=0; i < projects.length; i++) {   
+         if (projects[i].available === true) {
         domString += `<div class="card my-2" style="width: 18rem;" id="${i}">  
         <p class="title">${projects[i].title}</p>
         <img src=${projects[i].url} class="card-img-top" 
@@ -62,11 +62,19 @@ for (let i=0; i < projects.length; i++) {
        <img src=${projects[i].githubUrl} class="card-img-bottom" alt="This is an image of ${projects[i].githubUrl}">
        </div>
        </div>`
-}   
-
-
+    }   
+       
+      }
   printToDom('projectsPage', domString)
+      
+      }
+  
+  const init = () => {
+   createProjectCards(projects);
+   
+  };
+  
+  init();
+  
 
-  }
-createProjectCards(projects);
     
